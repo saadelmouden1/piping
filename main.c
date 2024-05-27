f5aece00871e7294311c1f8bf7403f52131f2e1f
saad elmoudden <sel-moud@c2r3p7.1337.ma>
2024-05-27 20:48:27 +0100

    mm

---
+++ main.c
@@ -0,0 +1,42 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+
+int main(int argc, char **argv)
+{
+   
+
+    int fd[2];
+    if(pipe(fd) == -1)
+        return 1;
+    int pid1 = fork();
+    if(pid1 < 0)
+        return 2;
+    
+    if(pid1 == 0)
+    {
+        dup2(fd[1], STDOUT_FILENO);
+        close(fd[0]);
+        close(fd[1]);
+        execlp("ping", "ping", "-c","5", "google.com",NULL);
+       
+    }
+    int pid2 = fork();
+     if(pid2 < 0)
+        return 3;
+    if(pid2 == 0)
+    {
+        dup2(fd[0],STDIN_FILENO);
+        close(fd[1]);
+        close(fd[0]);
+        execlp("grep", "grep", "PING", NULL);
+    }
+    close(fd[1]);
+    close(fd[0]);
+    waitpid(pid1,NULL,0);
+    waitpid(pid2,NULL,0);
+    return 0;
+}
